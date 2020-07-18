9aae8f85daae581a1ff10036ee207c709ab9310b
Felipe Santos <felipemillhouse@gmail.com>
2020-07-18 19:19:35 -0400

    install libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
 
 // import { Container } from './styles';
 
-const src: React.FC = () => {
-  return <View />;
-};
+const App: React.FC = () => {
+  return <View />
+}
 
-export default src;
+export default App
