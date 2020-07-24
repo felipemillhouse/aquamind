d73eb16c1c9264c377201e4e89800b676e26eb39
Felipe Santos <felipemillhouse@gmail.com>
2020-07-23 23:41:08 -0400

    Transfer old files

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,19 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { StatusBar } from 'react-native'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_300Light,
-  Roboto_100Thin,
-} from '@expo-google-fonts/roboto'
 
+import AppLoading from 'View/@Components/FakeLoadingScreen'
 import { store, persistor } from './store'
-import AppLoading from './View/@Components/FakeLoadingScreen'
 import Loading from './View/@Components/Loading'
 import Alert from './View/@Components/Alert'
 import theme from './View/@Theme'
 import Routes from './routes'
 
 const App: React.FC = () => {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_300Light,
-    Roboto_100Thin,
-  })
-
-  if (!fontsLoaded) {
-    return <AppLoading />
-  }
   return (
     <Provider store={store}>
       <PaperProvider theme={theme}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={<AppLoading />} persistor={persistor}>
           <StatusBar barStyle="light-content" />
           <Routes />
         </PersistGate>
