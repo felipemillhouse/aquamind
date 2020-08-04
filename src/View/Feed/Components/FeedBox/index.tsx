d7d9fd58bd51fccbb14d06acafae634db27e8fce
Felipe Santos <felipemillhouse@gmail.com>
2020-08-03 22:46:51 -0400

    optimize panGesture

--- src/View/Feed/Components/FeedBox/index.tsx
+++ src/View/Feed/Components/FeedBox/index.tsx
@@ -1,17 +1,21 @@
 // resources: https://www.youtube.com/watch?v=MukiK57qwVY
 
-import React, { useState } from 'react'
-import { Animated, StyleSheet, View, Alert } from 'react-native'
+import React from 'react'
+import { StyleSheet, View, Alert } from 'react-native'
+import { State, PinchGestureHandler } from 'react-native-gesture-handler'
+import Animated, { block, cond, eq, set, useCode } from 'react-native-reanimated'
 import {
-  PinchGestureHandler,
-  State,
-  PinchGestureHandlerStateChangeEvent,
-  PinchGestureHandlerGestureEvent,
-} from 'react-native-gesture-handler'
-import defaultAvatar from '../../../../assets/Avatar.png'
+  pinchActive,
+  pinchBegan,
+  transformOrigin,
+  translate,
+  vec,
+  spring,
+  usePinchGestureHandler,
+} from 'react-native-redash'
 
+import defaultAvatar from '../../../../assets/Avatar.png'
 import theme from '../../../Theme'
-import { FeedImage } from './styles'
 import UserHeader from '../UserHeader'
 import Footer from '../Footer'
 import { FeedState } from '../../../../store/feed'
@@ -21,50 +25,37 @@ type FeedBoxProps = {
   feed: FeedState
 }
 export default ({ feed }: FeedBoxProps) => {
-  const scale = new Animated.Value(1)
   const ratioWidth = theme.sizes.width
   const ratioHeight = theme.sizes.width * (feed.height / feed.width)
-  const [zIndex, setZIndex] = useState(2)
-  const tX = new Animated.Value(0)
-  const tY = new Animated.Value(0)
 
-  const onPinchEvent = Animated.event(
-    [
-      {
-        nativeEvent: {
-          scale,
-        },
-      },
-    ],
+  const { gestureHandler, numberOfPointers, state, scale, focal } = usePinchGestureHandler()
+  const origin = vec.createValue(0, 0)
+  const pinch = vec.createValue(0, 0)
+  const zIndex = cond(eq(state, State.ACTIVE), 10, 1)
+  const adjustedFocal = vec.add(
     {
-      useNativeDriver: true,
-      listener: (event: PinchGestureHandlerGestureEvent) => {
-        tX.setValue(event.nativeEvent.focalX - ratioWidth / 2)
-        tY.setValue(event.nativeEvent.focalY - ratioHeight / 2)
-      },
+      x: -ratioWidth / 2,
+      y: -ratioHeight / 2,
     },
+    focal,
   )
 
-  const onPinchStateChange = (event: PinchGestureHandlerStateChangeEvent) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      Animated.spring(scale, {
-        toValue: 1,
-        useNativeDriver: true,
-      }).start()
-      Animated.spring(tX, {
-        toValue: 0,
-        useNativeDriver: true,
-      }).start()
-      Animated.spring(tY, {
-        toValue: 0,
-        useNativeDriver: true,
-      }).start()
-      setTimeout(() => setZIndex(2), 500)
-    }
-    if (event.nativeEvent.state === State.ACTIVE) {
-      setZIndex(20)
-    }
-  }
+  useCode(
+    () =>
+      block([
+        cond(pinchBegan(state), vec.set(origin, adjustedFocal)),
+        cond(
+          pinchActive(state, numberOfPointers),
+          vec.set(pinch, vec.minus(vec.sub(origin, adjustedFocal))),
+        ),
+        cond(eq(state, State.END), [
+          set(pinch.x, spring({ from: pinch.x, to: 0, config: { mass: 0.5, damping: 10 } })),
+          set(pinch.y, spring({ from: pinch.y, to: 0, config: { mass: 0.5, damping: 10 } })),
+          set(scale, spring({ from: scale, to: 1, config: { mass: 0.5, damping: 10 } })),
+        ]),
+      ]),
+    [adjustedFocal, numberOfPointers, origin, pinch, scale, state],
+  )
 
   return (
     <>
@@ -74,23 +65,20 @@ export default ({ feed }: FeedBoxProps) => {
         dimensions={feed.dimensions}
         date={feed.date}
       />
-      <PinchGestureHandler
-        onGestureEvent={onPinchEvent}
-        onHandlerStateChange={event => onPinchStateChange(event)}
-      >
+      <PinchGestureHandler {...gestureHandler}>
         <Animated.View style={{ width: ratioWidth, height: ratioHeight, zIndex }}>
           <DoubleTap style={{ zIndex: 2 }} onPress={() => Alert.alert('Like the picture function')}>
-            <FeedImage
+            <Animated.Image
               source={{ uri: feed.photoUrl }}
-              width={feed.width}
-              height={feed.height}
               style={{
-                transform: [{ scale }, { translateX: tX }, { translateY: tY }],
-                zIndex: 2,
+                transform: [...translate(pinch), ...transformOrigin(origin, { scale })],
+                ...StyleSheet.absoluteFillObject,
+                width: ratioWidth,
+                height: ratioHeight,
               }}
             />
           </DoubleTap>
-          <View style={[StyleSheet.absoluteFill, { backgroundColor: theme.colors.background }]} />
+          <View style={{ flex: 1, backgroundColor: theme.colors.background }} />
         </Animated.View>
       </PinchGestureHandler>
       <Footer liked likes={feed.likes} comments={feed.comments} />
